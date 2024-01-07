c875f96c5bb35c8f03b4f49d1c80d0bc56ed0cd3
AdekanIT <adekankain@gmail.com>
2024-01-07 11:16:41 +0500

    Data for Bot

---
+++ buttons.py
@@ -0,0 +1,29 @@
+from telebot import types
+
+
+def num_bt():
+    # Create environment
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # Create button
+    number = types.KeyboardButton('Sent phone number', request_contact=True)
+    # Add button in environment
+    kb.add(number)
+    return kb
+
+# Buttons for sent location
+def loc_bt():
+    # Create environment
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # Create button
+    location = types.KeyboardButton('Sent location', request_location=True)
+    # Add button in environment
+    kb.add(location)
+    return kb
+
+
+
+
+
+
+
+
